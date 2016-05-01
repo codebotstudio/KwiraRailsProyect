1e5f1b9b2c9a5077ca712df06e18054d26c07568
Daniel Monroy <ldanielmonroy@gmail.com>
2016-05-01 10:53:03 -0500

    Exitoso total de venta actualizado automáticamente

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,6 +1,7 @@
 class Sale < ActiveRecord::Base
   belongs_to :user
   after_create :save_product_id
+  after_create :save_total
   has_many :has_product
   has_many :products, through: :has_product
 
@@ -15,6 +16,18 @@ class Sale < ActiveRecord::Base
   	@product_id.each do |product_id|
   		HasProduct.create(product_id: product_id, sale_id: self.id)
   	end
+  end
+
+  def save_total
+    item = 0
+    total = 0
+    self.products.each do |product|
+      item += 1
+      total += product.sale_price
+    end
+    self.items = item
+    self.total_price = total
+    self.save
   end
 
 end
