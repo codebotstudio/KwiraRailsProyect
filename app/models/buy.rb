6ecc1ae67f199f7acaf063426635bda6a7fb0d87
Daniel Monroy <ldanielmonroy@gmail.com>
2016-05-15 11:43:16 -0500

    Preparandose para el merge sale-q

--- app/models/buy.rb
+++ app/models/buy.rb
@@ -1,6 +1,7 @@
 class Buy < ActiveRecord::Base
   belongs_to :user
   after_create :save_product_id
+#  after_create :update_stock
   has_many :has_new_product
   has_many :products, through: :has_new_product
 
@@ -18,5 +19,14 @@ class Buy < ActiveRecord::Base
   		HasNewProduct.create(product_id: product_id, buy_id: self.id)
   	end
   end
+
+=begin
+  def update_stock
+  	@product_id.each do |product_id|
+  		@product = Product.find(product_id)
+  		@product.units += 1
+  	end
+  end
+=end
 
 end
