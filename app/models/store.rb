0b0b0c0a377a7d9bb36db2669779eb4b50655f78
Daniel Monroy <danielmonroy@iMac.local>
2016-08-20 16:13:37 -0500

    finalizado

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,6 @@
 class Store < ActiveRecord::Base
+	validates :name, presence: true
+	validates :address, presence: true
+	validates :phone, presence: true
+	validates :wholesale, presence: true
 end
